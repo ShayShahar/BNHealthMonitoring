e5a74156304fd8f347cb4b1716a37a9a8f003664
ShayShahar <shayscal90@gmail.com>
2018-01-05 12:47:20 +0200

    fix scrolling issues

--- Source/BNHealthMonitoring.UI/Model/Link.cs
+++ Source/BNHealthMonitoring.UI/Model/Link.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Animation;
 using Caliburn.Micro;
 
 namespace BNHealthMonitoring.UI.Model
 {
     public class Link : PropertyChangedBase
     {
+        private double m_probability;
+
         public Component ResponsibleComponent { get; set; }
-        public double Probability{ get; set; }
+
+        public double Probability
+        {
+            get
+            {
+                return m_probability;
+            }
+            set { m_probability = Math.Round(value, 2); }
+        }
+
         public string DestenationName { get; set; }
         public string State { get; set; }
 
